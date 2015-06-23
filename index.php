b2ee3f583a2b50a8571d2c42292d162195793cae
Changastyle <nico.grossi4@gmail.com>
2015-06-23 10:48:11 -0300

    Se a avanzado un poco con la UI

--- index.php
+++ index.php
@@ -7,11 +7,27 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Welcome to Nicolas Grossi billboard</title>
+        <link rel="stylesheet" href="frontend/css/general.css">
+        <link rel="stylesheet" href="frontend/css/index.css">
+        <!--<link rel="stylesheet" href="frontend/css/general.css">-->
+        <?php
+         echo $GLOBALS;
+        ?>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="paneles" id="banner"></div>
+        <div class="paneles" id="body">
+               <div class="paneles" id="panelPrincipal">
+                         asdfasdfasf
+                         <footer class="paneles" id="footer">FOOTERRRR</div>
+               </div>
+            <div class="paneles panelesLaterales" id="panelIZQ"></div>
+            <div class="paneles panelesLaterales" id="panelDER"></div>
+            
+            
+        </div>
+
+           
     </body>
 </html>
